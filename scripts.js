9177f45f82e7c4832f0376a5a9eba724e73bb49c
Gabriel <gabrielfalves2003@gmail.com>
2022-04-13 12:29:13 -0300

    tela de entrada

--- scripts.js
+++ scripts.js
@@ -1,3 +1,39 @@
+let nomeUsuario;
+
+function entraChat(){
+
+    let nome = document.querySelector(".tela-entrada input");
+    nomeUsuario = nome.value;
+    
+    let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", {name: nomeUsuario});
+
+    promise.catch(trataErroEntrada);
+    promise.then(trataSucessoEntrada);
+}
+
+function trataErroEntrada(error){
+
+    if(error.response.status === 400){
+        document.querySelector(".tela-entrada p").classList.remove("escondido");
+    }
+}
+
+function trataSucessoEntrada(response){
+    
+    document.querySelector(".tela-entrada").classList.add("escondido");
+    document.querySelector(".container").classList.remove("escondido");
+    carregaMensagens();
+    manterConexao();
+}
+
+function carregaMensagens(){
+}
+
+function manterConexao(){
+}
+
+
+
 function abreMenu(){
     let fundoPreto = document.querySelector(".fundo-preto");
     fundoPreto.classList.remove("escondido");
